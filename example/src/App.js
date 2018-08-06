4974f6ea3c50cb5730ebea58af173b47e8d984e5
Olivier Mangin <aicel@make-it.fr>
2018-08-06 15:56:08 +0200

    change images url to absolute

--- example/src/App.js
+++ example/src/App.js
@@ -8,11 +8,11 @@ export default class App extends Component {
       <div>
         <ExampleComponent
           images={[
-            {src: '/road-1072823_960_720.jpg'},
-            {src: '/green-1072828_960_720.jpg'},
-            {src: '/mountain-landscape-2031539_960_720.jpg'},
-            {src: '/beach-1867285_960_720.jpg'},
-            {src: '/nature-3082832_960_720.jpg'},
+            {src: 'https://abs0lom.github.io/react-3dgallery/road-1072823_960_720.jpg'},
+            {src: 'https://abs0lom.github.io/react-3dgallery/green-1072828_960_720.jpg'},
+            {src: 'https://abs0lom.github.io/react-3dgallery/mountain-landscape-2031539_960_720.jpg'},
+            {src: 'https://abs0lom.github.io/react-3dgallery/beach-1867285_960_720.jpg'},
+            {src: 'https://abs0lom.github.io/react-3dgallery/nature-3082832_960_720.jpg'},
           ]}
           backButton={({className}) => <button className={className} onClick={() => window.location.href = "https://github.com/abs0lom/react-3dgallery"}>Back</button>}
         />
